fe26531633e856687065a352e082e2ba25d1d934
Asmit <iamasmit1919@gmail.com>
2025-03-15 11:54:30 +0530

    update_v2_0.4
    Section 3 Base added + Bugs Fixed

---
+++ src/app/components/Section_3.tsx
@@ -0,0 +1,11 @@
+import "@/app/styles/section_3.scss";
+
+const Section_3 = () => {
+  return (
+    <div className="section_3">
+      <div className="section_3__cont">IDK WHAT</div>
+    </div>
+  );
+};
+
+export default Section_3;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import "@/app/styles/globals.scss";
 import Section_0 from "./components/Section_0";
 import Section_1 from "./components/Section_1";
-import "@/app/styles/globals.scss";
 import Section_2 from "./components/Section_2";
+import Section_3 from "./components/Section_3";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Section_0 />
       <Section_1 />
       <Section_2 />
+      <Section_3 />
     </>
   );
 };
