ffe4c1483c7c6346bb91a8b6dc5e474c0bbd17fc
Asmit <iamasmit1919@gmail.com>
2025-04-20 11:32:11 +0530

    update_v2_0.11.2
    Added Not Found Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import "@/app/styles/globals.scss";
+import { useRouter } from "next/navigation";
+// import Image from "next/image";
+
 export default function NotFound() {
+  const router = useRouter();
+
+  const homepageLink = () => {
+    router.push("/");
+  };
+
   return (
-    <div>
-      <h1>WTF GO BACK WHERE YOU CAME FROM!!!!!</h1>
-      <p>WRONG PATH BUDDY</p>
+    <div className="not_found" onClick={homepageLink}>
+      <div className="not_found_cont">
+        <div className="not_found_cont_contents">
+          <div className="not_found_cont_contents_heading">
+            <h1>404</h1>
+          </div>
+          <div className="not_found_cont_contents_text">
+            <p className="n_f_c_contents_text_main">Off The Map It Seems</p>
+            <p className="n_f_c_contents_text_sub">
+              Here&lsquo;s Nothing But Ocean Drifts
+            </p>
+          </div>
+          <div className="not_found_cont_cta">
+            <p>Back to Home Port</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
-
+}
